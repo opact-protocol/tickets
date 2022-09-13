f349a7fbd1251c3fb9d57dc0fd458863c440bf72
p-destri <pd.destri@gmail.com>
2022-09-13 10:05:43 +0100

    refact/ removal from blacklist

--- packages/contracts/contract/src/actions/blacklist.rs
+++ packages/contracts/contract/src/actions/blacklist.rs
@@ -1,4 +1,8 @@
-use crate::{*, serial_hasher::serial_hash, events::event_white_list_update};
+use crate::{
+  *,
+  serial_hasher::serial_hash,
+  events::{event_white_list_update, event_black_list_removal},
+};
 use near_sdk::{env, near_bindgen, PanicOnDefault, AccountId, BorshStorageKey, json_types::U64};
 use near_bigint::U256;
 
@@ -15,10 +19,23 @@ impl Contract {
     let account_hash: U256 = serial_hash(U256::zero(), account.to_string().as_str());
 
     let index = self.white_list.add_to_blacklist(account_hash);
-    //event_new_white_list(U64(self.white_list.), account_hash);
+
     if let Some(index) = index {
-      event_white_list_update(U64(index), account_hash);
+      event_white_list_update(U64(index), self.white_list.zeros(0));
     }
+  }
+
+  pub fn remove_blacklist(&mut self, account: AccountId) {
+    assert!(
+      self.guardian.contains(&account),
+      "This account is not a registred guardian for the blacklist"
+    );
+
+    let account_hash: U256 = serial_hash(U256::zero(), account.to_string().as_str());
+
+    self.white_list.remove_from_blacklist(account_hash);
+
+    event_black_list_removal(account);
   }
 
   pub fn add_guardian(&mut self, account_to_become_guardian: AccountId) {
