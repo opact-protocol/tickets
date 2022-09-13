c217eb550eb9eb1ef7c5dfaae48ae229e628b297
Jasso <veigajoaoa@gmail.com>
2022-09-13 14:34:37 -0300

    progress

--- packages/contracts/contract/src/actions/view.rs
+++ packages/contracts/contract/src/actions/view.rs
@@ -1,10 +1,15 @@
 use crate::*;
 use near_bigint::U256;
 use crate::hashes::account_hash;
+use mimc::u256_mimc_sponge_single;
 
 #[near_bindgen]
 impl Contract {
   pub fn view_a_hash(&self, account_id: AccountId) -> U256 {
     account_hash(&account_id)
+  }
+
+  pub fn view_nullifier_hash(&self, nullifier: U256) -> U256 {
+    u256_mimc_sponge_single(U256::zero(), [nullifier])[0]
   }
 }
