c67b4b18adfecf35430d9a51e09b9ad7bc5151de
Mateus Santana <42101423+1Mateus@users.noreply.github.com>
2022-11-23 15:37:19 -0300

    feat(front): fix tree

--- packages/front/src/services/build_tree.ts
+++ packages/front/src/services/build_tree.ts
@@ -1,6 +1,6 @@
-import { MerkleTree } from 'fixed-merkle-tree';
-import { mimc } from './mimc';
-import { nearRpcClient } from './near_rpc';
+import { MerkleTree } from "fixed-merkle-tree";
+import { mimc } from "./mimc";
+import { nearRpcClient } from "./near_rpc";
 
 const EVENT_PREFIX = "EVENT_JSON:";
 const EVENT_STANDARD = "hideyour.cash";
@@ -8,56 +8,87 @@ const EVENT_STANDARD = "hideyour.cash";
 const VERCEL_URL = "https://monorepo-hideyour-cash.vercel.app/api/query";
 
 const MERKLE_TREE_OPTIONS = {
-    zeroElement: '21663839004416932945382355908790599225266501822907911457504978515578255421292',
-    hashFunction: mimc.hash
+  zeroElement:
+    "21663839004416932945382355908790599225266501822907911457504978515578255421292",
+  hashFunction: mimc.hash,
+};
+
+const eventTypes = {
+  deposit: "deposit",
+  updated_whitelist: "whitelist",
 };
 
 export async function buildTree() {
-    await mimc.initMimc();
-    const txResponse = await fetch(VERCEL_URL);
-    const { body: transactions } = await txResponse.json();
+  await mimc.initMimc();
+  const txResponse = await fetch(VERCEL_URL);
+  const { body: transactions } = await txResponse.json();
 
-    const finalExecutionOutcomes = await Promise.all(transactions.map(
-        ({
-            transaction_hash: txHash,
-            predecessor_account_id: signerId
-        }) => (
-            nearRpcClient.connection.provider.txStatusReceipts(txHash, signerId)
-        )
-    ));
+  const finalExecutionOutcomes = await Promise.all(
+    transactions.map(
+      ({ transaction_hash: txHash, predecessor_account_id: signerId }) =>
+        nearRpcClient.connection.provider.txStatusReceipts(txHash, signerId)
+    )
+  );
 
-    const logs = finalExecutionOutcomes
-        .flatMap(feo => feo.receipts_outcome.flatMap(receipt => receipt.outcome.logs))
-        .filter(logs => logs.length > 0);
+  const logs = finalExecutionOutcomes
+    .flatMap((feo) =>
+      feo.receipts_outcome.flatMap((receipt) => receipt.outcome.logs)
+    )
+    .filter((logs) => logs.length > 0);
 
-    const events = logs
-        .map(log => log.slice(EVENT_PREFIX.length))
-        .map(log => JSON.parse(log))
-        .filter(event => event.standard = EVENT_STANDARD)
-        .filter(event => event.version = '1.0.0');
+  const events = logs
+    .map((log) => log.slice(EVENT_PREFIX.length))
+    .map((log) => JSON.parse(log))
+    .filter((event) => (event.standard = EVENT_STANDARD))
+    .filter((event) => (event.version = "1.0.0"))
+    .reduce(
+      (res, event) => {
+        const eventType = eventTypes[event.event];
 
-    const commitmentsTree = new MerkleTree(20, [], MERKLE_TREE_OPTIONS);
-    const whitelistTree = new MerkleTree(20, [], MERKLE_TREE_OPTIONS);
-
-    for (const event of events) {
-        switch (event.event) {
-            case 'deposit': {
-                const { index, value } = event.data[0];
-
-                commitmentsTree.update(index, value);
-
-                break;
-            }
-
-            case 'updated_whitelist': {
-                const { index, value } = event.data[0];
-
-                whitelistTree.update(index, value);
-
-                break;
-            }
+        if (!eventType) {
+          return res;
         }
-    }
 
-    return { commitmentsTree, whitelistTree };
+        res[eventTypes[event.event]].push(event);
+
+        return res;
+      },
+      {
+        deposit: [],
+        whitelist: [],
+      }
+    );
+
+  events.deposit.sort((a, b) =>
+    Number(a.data[0].index) > Number(b.data[0].index) ? 1 : -1
+  );
+  events.whitelist.sort((a, b) =>
+    Number(a.data[0].index) > Number(b.data[0].index) ? 1 : -1
+  );
+
+  const commitmentsTree = new MerkleTree(20, [], MERKLE_TREE_OPTIONS);
+
+  events.deposit.forEach(({ data }) => {
+    const { index, value } = data[0];
+
+    try {
+      commitmentsTree.update(index, value);
+    } catch (e) {
+      console.warn(e);
+    }
+  });
+
+  const whitelistTree = new MerkleTree(20, [], MERKLE_TREE_OPTIONS);
+
+  events.whitelist.forEach(({ data }) => {
+    const { index, value } = data[0];
+
+    try {
+      whitelistTree.update(index, value);
+    } catch (e) {
+      console.warn(e);
+    }
+  });
+
+  return { commitmentsTree, whitelistTree };
 }
