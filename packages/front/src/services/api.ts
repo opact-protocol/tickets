ef2957ec51aa64e2f847b8d135e17a498b422df8
Mateus Santana <42101423+1Mateus@users.noreply.github.com>
2022-12-06 18:43:54 -0300

    Update api.ts

--- packages/front/src/services/api.ts
+++ packages/front/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://seal-app-xcfqc.ondigitalocean.app",
+  baseURL: "https://seal-app-xcfqc.ondigitalocean.app/",
 });
 
 export default api;
