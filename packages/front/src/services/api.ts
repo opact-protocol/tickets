d96205e4ca890b7edfbc5ebfdbb9da2589457f62
Mateus Santana <42101423+1Mateus@users.noreply.github.com>
2022-12-06 19:15:31 -0300

    Update api.ts

--- packages/front/src/services/api.ts
+++ packages/front/src/services/api.ts
@@ -5,7 +5,6 @@ export const api = axios.create({
   headers: {
     "Content-Type": "application/json",
     "Access-Control-Allow-Origin": "*",
-    "Access-Control-Request-Headers": "Access-Control-Request-Headers",
   },
 });
 
