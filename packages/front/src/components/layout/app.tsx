932500baa4faf649e9899efc35acae0a26ff08b0
Mateus Santana <42101423+1Mateus@users.noreply.github.com>
2022-12-08 11:42:54 -0300

    feat(front): requested changes

--- packages/front/src/components/layout/app.tsx
+++ packages/front/src/components/layout/app.tsx
@@ -1,5 +1,7 @@
 import routes from "virtual:generated-pages-react";
+import { useState } from "react";
 import { Header } from "./header";
+import { AboutUsModal } from "../modals";
 import { Feedback } from "./feedback";
 import { BrowserRouter as Router, useRoutes } from "react-router-dom";
 
@@ -7,10 +9,25 @@ const Pages = () => {
   return useRoutes(routes);
 };
 
-export const App = () => (
-  <Router>
-    <Header />
-    <Pages />
-    <Feedback />
-  </Router>
-);
+// export const handleOpenModal = (callback: (value: boolean) => void) => {
+//   if (localStorage.getItem("@hyc-first-interaction")) {
+//     return;
+//   }
+
+//   callback(true);
+
+//   localStorage.setItem("@hyc-first-interaction", "false");
+// };
+
+export const App = () => {
+  const [showModal, setShowModal] = useState(true);
+
+  return (
+    <Router>
+      <Header />
+      <Pages />
+      <AboutUsModal isOpen={showModal} onClose={() => setShowModal(false)} />
+      <Feedback />
+    </Router>
+  );
+};
---
+++ packages/front/src/components/modals/about-us.tsx
@@ -0,0 +1,104 @@
+import { useApplication } from "@/store";
+import { Fragment, useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { useWalletSelector } from "@/utils/context/wallet";
+import { XMarkIcon } from "@heroicons/react/24/solid";
+
+export const AboutUsModal = ({
+  isOpen,
+  onClose,
+}: {
+  isOpen: boolean;
+  onClose: () => void;
+}) => {
+  const [userAddress, setUserAddress] = useState("");
+  const { selector } = useWalletSelector();
+
+  const { sendWhitelist } = useApplication();
+
+  const apply = () => {
+    if (!userAddress) {
+      return;
+    }
+
+    sendWhitelist(selector, userAddress);
+  };
+
+  return (
+    <Transition appear show={isOpen} as={Fragment}>
+      <Dialog as="div" className="relative z-10" onClose={onClose}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-black bg-opacity-25" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <Dialog.Panel className="w-full max-w-xl transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all space-y-4 relative">
+                <button
+                  onClick={() => onClose()}
+                  className="absolute right-[24px] top-[24px] hover:opacity-[0.7]"
+                >
+                  <XMarkIcon className="text-[#121315] w-[24px]" />
+                </button>
+
+                <Dialog.Title
+                  as="span"
+                  className="text-[#121315] text-[18px] font-[500]"
+                >
+                  Hideyour.cash
+                </Dialog.Title>
+
+                <div className="mt-2 text-[16px] text-[#121315] space-y-[12px]">
+                  <p>Welcome to hideyour.cash MVP!</p>
+
+                  <p>
+                    This is a privacy transactions platform currently deployed
+                    on NEAR testnet.
+                  </p>
+
+                  <p>
+                    To try it, you'll need to: connect your wallet, add yourself
+                    to the allowlist and then you'll be able to deposit and
+                    withdraw NEAR tokens from your NEAR testnet wallet.
+                  </p>
+
+                  <p>
+                    Please, watch the tutorial to understand how the platform
+                    works and to properly test it.
+                  </p>
+
+                  <p>
+                    After the test, we'll be pleased to get your feedback.
+                    You'll find a button on the page to take you to the form.
+                  </p>
+
+                  <p>
+                    Furthermore, thank you very much for being part of this with
+                    us since the beginning!
+                  </p>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition>
+  );
+};
