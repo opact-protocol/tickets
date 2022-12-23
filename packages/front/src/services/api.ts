9314fa540d15c3bfbbae71edcb663008af0cc7fa
Mateus Santana <42101423+1Mateus@users.noreply.github.com>
2022-12-23 16:54:47 -0300

    feat(front): update vite relayer

--- packages/front/src/services/api.ts
+++ packages/front/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://seal-app-xcfqc.ondigitalocean.app/",
+  baseURL: import.meta.env.VITE_RELAYER,
   headers: {
     "Content-Type": "application/json",
     "Access-Control-Allow-Origin": "*",
