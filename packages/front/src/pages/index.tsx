eb3ba296a53c51478746b016012b8c8e815cda52
Alexandre <alexandre01393@gmail.com>
2023-01-23 16:22:59 -0300

    feat(front): app redesign

--- packages/front/src/pages/index.tsx
+++ packages/front/src/pages/index.tsx
@@ -2,7 +2,7 @@ import { useId } from "react";
 import { Container, Actions } from "@/components";
 
 function BackgroundIllustration(props) {
-  let id = useId();
+  const id = useId();
 
   return (
     <div {...props}>
@@ -74,7 +74,7 @@ export function Index() {
   return (
     <div className="overflow-hidden relative py-20 sm:py-32 lg:pb-32 xl:pb-36">
       <Container>
-        <div className="relative pt-10">
+        <div className="relative">
           <Actions />
           <BackgroundIllustration className="absolute left-1/2 top-[-100px] h-[1200px] w-[1200px] -translate-x-1/3 stroke-gray-300/70 [mask-image:linear-gradient(to_bottom,white_20%,transparent_75%)] sm:-translate-x-1/2 lg:ml-12 xl:ml-0 z-[1]" />
         </div>
