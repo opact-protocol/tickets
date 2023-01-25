408f18d3bcb73c6adda5a8b1a848f7d2ff1832cf
Alexandre <alexandre01393@gmail.com>
2023-01-25 19:46:51 -0300

    feat(front): redesign modals

---
+++ packages/front/src/hooks/useAction.ts
@@ -0,0 +1,39 @@
+import { getTransactionsAction, getTransactionState } from "@/utils/tools";
+import { useEffect, useState } from "react";
+
+interface ActionProps {
+  status: string;
+  message: string;
+  methodName: string;
+}
+
+export const useAction = (transactionHashes: string, accountId: string) => {
+  const [action, setAction] = useState<ActionProps>();
+
+  useEffect(() => {
+    if (!accountId || !transactionHashes) {
+      return;
+    }
+
+    (async () => {
+      const transactions = transactionHashes.split(",");
+
+      const states: any[] = [];
+
+      for (let i = 0; i < transactions.length; i++) {
+        const state = await getTransactionState(transactions[i], accountId);
+        states.push(state);
+      }
+
+      const action = getTransactionsAction(states);
+
+      if (!action) {
+        return;
+      }
+
+      setAction(action);
+    })();
+  }, [accountId]);
+
+  return { action };
+};
