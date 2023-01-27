d8f08dd179a99ccebb6a1f7d6cf2222b5f44cd18
Alexandre <alexandre01393@gmail.com>
2023-01-26 21:45:23 -0300

    feat(front): add slider and mock in withdraw tab

--- packages/front/src/components/layout/app.tsx
+++ packages/front/src/components/layout/app.tsx
@@ -1,5 +1,6 @@
 import routes from "virtual:generated-pages-react";
 import { BrowserRouter as Router, useRoutes } from "react-router-dom";
+import { NeedHelp } from "./needHelp";
 
 const Pages = () => {
   return useRoutes(routes);
--- packages/front/tailwind.config.js
+++ packages/front/tailwind.config.js
@@ -109,6 +109,9 @@ module.exports = {
       boxShadow: {
         sm: "0px 4px 10px rgba(96, 108, 210, 0.2), 0px 2px 4px rgba(96, 108, 210, 0.35)",
       },
+      boxShadow: {
+        sm: "0px 4px 10px rgba(96, 108, 210, 0.2), 0px 2px 4px rgba(96, 108, 210, 0.35)",
+      },
     },
   },
   plugins: [],
