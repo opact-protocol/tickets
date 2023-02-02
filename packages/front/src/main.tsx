bae2dbc778ad0dd684a220cde91289df86010cdb
Alexandre <alexandre01393@gmail.com>
2023-02-02 17:51:40 -0300

    fix(front): moving large file

--- packages/front/src/main.tsx
+++ packages/front/src/main.tsx
@@ -5,17 +5,21 @@ import ReactDOM from "react-dom/client";
 import { Toaster } from "react-hot-toast";
 import { WalletSelectorContextProvider } from "@/utils/context/wallet";
 import { WalletSelectorModal } from "@/components/modals/wallet";
+import { ApolloProvider } from "@apollo/client";
 import Buffer from "node:buffer";
+import { client } from "./services/graphqlClient";
 
 // TODO: Find a better way to handle this buffer error
 window.Buffer = window.Buffer || Buffer;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <WalletSelectorContextProvider>
-      <App />
-      <Toaster position="bottom-right" reverseOrder={false} />
-      <WalletSelectorModal />
-    </WalletSelectorContextProvider>
+    <ApolloProvider client={client}>
+      <WalletSelectorContextProvider>
+        <App />
+        <Toaster position="bottom-right" reverseOrder={false} />
+        <WalletSelectorModal />
+      </WalletSelectorContextProvider>
+    </ApolloProvider>
   </React.StrictMode>
 );
