ed142ad6f100771564a6767145678f3f16473b8d
Jasso <veigajoaoa@gmail.com>
2023-02-07 12:09:36 -0300

    feat(contracts): add protocol fees
    adds immutable fee upon HYC contract initialization
    adds upgrading contract structures to registry to allow for deprecating old contracts

--- packages/contracts/contract/src/actions/owner.rs
+++ packages/contracts/contract/src/actions/owner.rs
@@ -1,17 +1,10 @@
-use near_sdk::{env, near_bindgen, assert_one_yocto};
-
-use crate::{Contract, ContractExt};
+use crate::*;
 
 #[near_bindgen]
 impl Contract {
   #[payable]
   pub fn toggle_kill_switch(&mut self) {
-    assert_eq!(
-      env::predecessor_account_id(),
-      self.owner,
-      "only owner can call"
-    );
-    assert_one_yocto();
+    self.only_owner();
     self.kill_switch = !self.kill_switch;
   }
 }
