5f2fed81b1f53aa42e0282a4fa815fd454cdc45b
Jasso <veigajoaoa@gmail.com>
2023-02-08 18:15:36 -0300

    start registry tests

--- packages/contracts/registry/src/lib.rs
+++ packages/contracts/registry/src/lib.rs
@@ -108,3 +108,167 @@ impl Contract {
     assert_one_yocto();
   }
 }
+
+#[cfg(test)]
+mod tests {
+  pub use near_sdk::{testing_env, Balance, MockedBlockchain, VMContext, Gas};
+  pub use near_sdk::{VMConfig, RuntimeFeesConfig};
+  pub use near_sdk::test_utils::{get_logs, get_created_receipts};
+  pub use near_sdk::mock::{VmAction};
+  pub use near_sdk::serde_json::{json};
+  pub use near_sdk::collections::{LazyOption};
+
+  pub use std::panic::{UnwindSafe, catch_unwind};
+  pub use std::collections::{HashMap};
+  pub use std::convert::{TryFrom, TryInto};
+  pub use std::str::{from_utf8};
+
+  pub use super::*;
+
+  const CONTRACT: &str = "contract.testnet";
+  const OWNER: &str = "owner.testnet";
+  const NON_OWNER: &str = "other.testnet";
+  const CURRENCY_1: &str = "currency.testnet";
+  const CURRENCY_2: &str = "currency2.testnet";
+  const HYC: &str = "hyc.testnet";
+
+  pub fn get_context(
+    input: Vec<u8>,
+    attached_deposit: u128,
+    account_balance: u128,
+    signer_id: AccountId,
+    block_timestamp: u64,
+    prepaid_gas: Gas,
+  ) -> VMContext {
+    VMContext {
+      current_account_id: CONTRACT.parse().unwrap(),
+      signer_account_id: signer_id.clone(),
+      signer_account_pk: vec![0; 33].try_into().unwrap(),
+      predecessor_account_id: signer_id.clone(),
+      input,
+      block_index: 0,
+      block_timestamp,
+      account_balance,
+      account_locked_balance: 0,
+      storage_usage: 0,
+      attached_deposit,
+      prepaid_gas,
+      random_seed: [0; 32],
+      view_config: None,
+      output_data_receivers: vec![],
+      epoch_height: 19,
+    }
+  }
+
+  pub fn init_contract() -> Contract {
+    Contract {
+      owner: OWNER.parse().unwrap(),
+      currencies_map: UnorderedMap::new(StorageKey::CurrenciesMap),
+      allowlist_set: UnorderedSet::new(StorageKey::AllowlistSet),
+    }
+  }
+
+  #[test]
+  fn test_add_entry_happy() {
+    let context = get_context(
+      vec![],
+      1,
+      0,
+      OWNER.parse().unwrap(),
+      0,
+      Gas(300u64 * 10u64.pow(12)),
+    );
+    testing_env!(context);
+
+    let currency = Currency::Near;
+    let amount = U256::from_dec_str("10").unwrap();
+
+    let mut contract = init_contract();
+    assert!(contract.currencies_map.len() == 0, "initialized with values");
+    contract.add_entry(currency.clone(), amount, HYC.parse().unwrap());
+
+    let entries_map = contract.currencies_map.get(&currency).unwrap();
+
+    assert_eq!(
+      entries_map.len(), 1
+    );
+    assert_eq!(
+      entries_map.get(&amount).unwrap().clone(),
+      HYC.parse::<AccountId>().unwrap()
+    )
+  }
+
+  #[test]
+  #[should_panic(expected = "This function is restricted to the owner")]
+  fn test_add_entry_non_owner() {
+    let context = get_context(
+      vec![],
+      1,
+      0,
+      NON_OWNER.parse().unwrap(),
+      0,
+      Gas(300u64 * 10u64.pow(12)),
+    );
+    testing_env!(context);
+
+    let currency = Currency::Near;
+    let amount = U256::from_dec_str("10").unwrap();
+
+    let mut contract = init_contract();
+    assert!(contract.currencies_map.len() == 0, "initialized with values");
+    contract.add_entry(currency.clone(), amount, HYC.parse().unwrap());
+
+  }
+
+  #[test]
+  #[should_panic(expected = "Requires attached deposit of exactly 1 yoctoNEAR")]
+  fn test_add_entry_non_yocto() {
+    let context = get_context(
+      vec![],
+      0,
+      0,
+      OWNER.parse().unwrap(),
+      0,
+      Gas(300u64 * 10u64.pow(12)),
+    );
+    testing_env!(context);
+
+    let currency = Currency::Near;
+    let amount = U256::from_dec_str("10").unwrap();
+
+    let mut contract = init_contract();
+    assert!(contract.currencies_map.len() == 0, "initialized with values");
+    contract.add_entry(currency.clone(), amount, HYC.parse().unwrap());
+
+  }
+
+  // #[test]
+  // fn test_remove_entry_happy() {}
+
+  // #[test]
+  // #[should_panic(expected = "This function is restricted to the owner")]
+  // fn test_remove_entry_non_owner() {}
+
+  // #[test]
+  // #[should_panic(expected = "Requires attached deposit of exactly 1 yoctoNEAR")]
+  // fn test_remove_entry_non_yocto() {}
+
+  // #[test]
+  // #[should_panic(expected = "Currency is not registered")]
+  // fn test_remove_entry_no_currency() {}
+
+  // #[test]
+  // #[should_panic(expected = "Amount was not registered for this currency")]
+  // fn test_remove_entry_no_amount() {}
+
+  // #[test]
+  // fn test_remove_from_allowlist_happy() {}
+
+  // #[test]
+  // #[should_panic(expected = "This function is restricted to the owner")]
+  // fn test_remove_from_allowlist_non_owner() {}
+
+  // #[test]
+  // #[should_panic(expected = "Requires attached deposit of exactly 1 yoctoNEAR")]
+  // fn test_remove_from_allowlist_non_yocto() {}
+}
