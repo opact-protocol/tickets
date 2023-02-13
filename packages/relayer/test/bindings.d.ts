552f32f345f27cee8421d58be5a3a1192798adf7
Mateus Santana <42101423+1Mateus@users.noreply.github.com>
2023-02-13 17:34:58 -0300

    feat(relayer): update teets

--- packages/relayer/test/bindings.d.ts
+++
@@ -1,5 +0,0 @@
-import { Env } from "../src/interfaces";
-
-interface Bindings extends Env {
-  USER_PROOF: string;
-}
--- packages/relayer/test/globals.d.ts
+++
@@ -1,5 +0,0 @@
-declare global {
-  function getMiniflareBindings(): Bindings;
-}
-
-export {};
