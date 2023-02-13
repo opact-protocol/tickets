67c8e674d37559042df37a50de30cbf8604d2c50
Mateus Santana <42101423+1Mateus@users.noreply.github.com>
2023-02-13 07:54:42 -0300

    feat(relayer): tests

--- packages/relayer/test/relayer.spec.ts
+++ packages/relayer/test/relayer.spec.ts
@@ -2,6 +2,37 @@ import { HEADERS } from "@/constants";
 import { fetch } from "@/services/router";
 import payload from "./json/payload.json";
 
+import { connect, KeyPair } from "near-api-js";
+import { InMemoryKeyStore } from "near-api-js/lib/key_stores";
+import { readInputs } from "./util";
+
+let user;
+
+const config = {
+  networkId: "testnet",
+  nodeUrl: "https://rpc.testnet.near.org",
+  walletUrl: "https://wallet.testnet.near.org",
+  helperUrl: "https://helper.testnet.near.org",
+  explorerUrl: "https://explorer.testnet.near.org",
+};
+
+beforeAll(async () => {
+  // const { HYC_CONTRACT } = getMiniflareBindings();
+
+  const { account } = readInputs();
+
+  const keyStore = new InMemoryKeyStore();
+  const keyPair = KeyPair.fromString(account.privateKey);
+  keyStore.setKey(config.networkId, account.accountid, keyPair);
+
+  user = await connect({
+    ...config,
+    keyStore,
+  });
+
+  console.log(user);
+});
+
 test("should return 402 - payload not is valid", async () => {
   const env = getMiniflareBindings();
 
@@ -75,36 +106,33 @@ test("should return 402 - Withdraw is not valid", async () => {
   );
 });
 
-test("should return 402 - Error on withdraw", async () => {
-  const env = getMiniflareBindings();
+// test("should return 402 - Error on withdraw", async () => {
+//   const env = getMiniflareBindings();
 
-  const baseRequest = new Request("http://localhost/relay", {
-    method: "POST",
-    headers: HEADERS,
-    body: JSON.stringify(payload),
-  });
+//   const baseRequest = new Request("http://localhost/relay", {
+//     method: "POST",
+//     headers: HEADERS,
+//     body: JSON.stringify(payload),
+//   });
 
-  const res = await fetch(baseRequest, env);
+//   const res = await fetch(baseRequest, env);
 
-  const textRes = await res.text();
+//   const textRes = await res.text();
 
-  expect(res.status).toBe(402);
-  expect(textRes).toContain('{"status":"failure","error":"Error on withdraw"}');
-});
+//   expect(res.status).toBe(402);
+//   expect(textRes).toContain('{"status":"failure","error":"Error on withdraw"}');
+// });
 
-test("should return 200 - success withdraw", async () => {
-  const env = getMiniflareBindings();
+// test("should return 200 - success withdraw", async () => {
+//   const env = getMiniflareBindings();
 
-  const baseRequest = new Request("http://localhost/relay", {
-    method: "POST",
-    headers: HEADERS,
-    body: JSON.stringify(payload),
-  });
+//   const baseRequest = new Request("http://localhost/relay", {
+//     method: "POST",
+//     headers: HEADERS,
+//     body: JSON.stringify(payload),
+//   });
 
-  const res = await fetch(baseRequest, env);
+//   const res = await fetch(baseRequest, env);
 
-  const textRes = await res.text();
-
-  expect(res.status).toBe(200);
-  expect(textRes).toContain('{"status":"success","error":"Error on withdraw"}');
-});
+//   expect(res.status).toBe(200);
+// });
