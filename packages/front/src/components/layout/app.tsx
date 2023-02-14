f5414546aae5045e00ad3f97b54419fe5a0d5f25
Mateus Santana <42101423+1Mateus@users.noreply.github.com>
2023-02-14 15:27:11 -0300

    feat(front): implement gtm

--- packages/front/src/components/layout/app.tsx
+++ packages/front/src/components/layout/app.tsx
@@ -1,11 +1,18 @@
 import routes from "virtual:generated-pages-react";
 import { BrowserRouter as Router, useRoutes } from "react-router-dom";
+import TagManager from "react-gtm-module";
 
 const Pages = () => {
   return useRoutes(routes);
 };
 
 export const App = () => {
+  const tagManagerArgs = {
+    gtmId: import.meta.env.VITE_GTM_ID as string,
+  };
+
+  TagManager.initialize(tagManagerArgs);
+
   return (
     <Router>
       <Pages />
