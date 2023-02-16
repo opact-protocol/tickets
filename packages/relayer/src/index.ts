b55a9e64a7dc9e0516678921c3b4182bd7a36c4c
Mateus Santana <42101423+1Mateus@users.noreply.github.com>
2023-02-16 18:35:21 -0300

    feat(relayer): update patch with router

--- packages/relayer/src/index.ts
+++ packages/relayer/src/index.ts
@@ -37,6 +37,8 @@ router.post("/relay", async ({ env, req, res }) => {
   res.body = body;
 });
 
+export const getRouter = () => router;
+
 // Listen Cloudflare Workers Fetch Event
 export default {
   async fetch(request: Request, env: Env): Promise<Response> {
