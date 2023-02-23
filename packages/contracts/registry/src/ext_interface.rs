5fcd23b3838465d7926f9cd978c2588aa9a82361
Jasso <veigajoaoa@gmail.com>
2023-02-23 20:15:24 -0300

    feat(contracts): update hapi.one connector

--- packages/contracts/registry/src/ext_interface.rs
+++ packages/contracts/registry/src/ext_interface.rs
@@ -1,10 +1,12 @@
-use near_sdk::{
-    ext_contract,
-    AccountId
-};
+use crate::*;
 
 #[ext_contract(ext_self)]
 pub trait SelfContract {
     fn allowlist_callback(account_id: AccountId);
     fn denylist_callback(account_id: AccountId);
+}
+
+#[ext_contract(ext_aml)]
+pub trait ExtAmlContract {
+    fn get_address(&self, address: AccountId) -> CategoryRisk;
 }
