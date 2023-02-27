9415e9b7d93f5087002034ce818aaf6ff33bdb96
Mateus Santana <42101423+1Mateus@users.noreply.github.com>
2023-02-27 11:41:27 -0300

    feat(sdk): update core lib

--- packages/sdk/vite.config.ts
+++ packages/sdk/vite.config.ts
@@ -1,16 +1,37 @@
-
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import packageJson from "./package.json";
 import stdLibBrowser from 'node-stdlib-browser';
 
-export default defineConfig({
+const getPackageName = () => {
+  return packageJson.name;
+};
+
+const getPackageNameCamelCase = () => {
+  try {
+    return getPackageName().replace(/-./g, (char) => char[1].toUpperCase());
+  } catch (err) {
+    throw new Error("Name property in package.json is missing.");
+  }
+};
+
+const fileName = {
+  es: `${getPackageName()}.mjs`,
+  cjs: `${getPackageName()}.cjs`,
+  iife: `${getPackageName()}.iife.js`,
+};
+
+const formats = Object.keys(fileName) as Array<keyof typeof fileName>;
+
+module.exports = defineConfig({
   base: "./",
   build: {
     target: 'esnext',
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
-      fileName: 'hideyour-cash-sdk',
-      formats: ['es'],
+      name: getPackageNameCamelCase(),
+      formats,
+      fileName: (format) => fileName[format],
     },
   },
   optimizeDeps: {
