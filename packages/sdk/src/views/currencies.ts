00cd8196478a6d5bd933881579d006642769dc48
Mateus Santana <42101423+1Mateus@users.noreply.github.com>
2023-02-28 08:12:31 -0300

    feat(sdk): update views

--- packages/sdk/src/views/currencies.ts
+++ packages/sdk/src/views/currencies.ts
@@ -18,6 +18,36 @@ export const viewCurrencyContracts = (
   return viewFunction(
     rpcUrl,
     contract,
-    'view_all_currencies',
+    'view_currency_contracts',
   );
 };
+
+export const viewIsContractAllowed = (
+  rpcUrl: string,
+  contract: string,
+  accountId: string,
+) => {
+  return viewFunction(
+    rpcUrl,
+    contract,
+    'view_is_contract_allowed',
+    {
+      account_id: accountId,
+    }
+  );
+}
+
+export const viewIsAllowlistRootValid = (
+  rpcUrl: string,
+  contract: string,
+  root: string,
+) => {
+  return viewFunction(
+    rpcUrl,
+    contract,
+    'view_is_allowlist_root_valid',
+    {
+      root,
+    }
+  );
+}
