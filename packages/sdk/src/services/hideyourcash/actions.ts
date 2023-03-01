dca3d2f8344db0eac603cd5c51f1a9be2fb05467
Mateus Santana <42101423+1Mateus@users.noreply.github.com>
2023-03-01 17:39:30 -0300

    feat(sdk): wip

--- packages/sdk/src/services/hideyourcash/actions.ts
+++ packages/sdk/src/services/hideyourcash/actions.ts
@@ -21,7 +21,6 @@ import type {
   RelayerDataInterface,
 } from "../../interfaces";
 import { Views } from "./views";
-// import { Account } from "near-api-js";
 
 export class Actions extends Views {
   readonly nodeUrl: string;
