5fc0f0ff16a82668f702e41de6315e0e3f078b26
Alexandre <alexandre01393@gmail.com>
2023-03-13 19:55:35 -0300

    feat(front): hook to update progress proof

--- packages/front/src/components/modals/loading/index.tsx
+++ packages/front/src/components/modals/loading/index.tsx
@@ -1,32 +1,4 @@
-import { useEffect, useState } from "react";
-
-export const LoadingModal = ({
-  generatingProof,
-}: {
-  generatingProof: boolean;
-}) => {
-  const [progress, setProgress] = useState<number>(0);
-  const totalTime = 90;
-  const updateInterval = 100;
-  const increment = 100 / ((totalTime * 1000) / updateInterval);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setProgress((prevProgress) => {
-        if (generatingProof) {
-          if (prevProgress === 99) {
-            clearInterval(interval);
-            return prevProgress;
-          }
-          return (prevProgress += increment);
-        } else {
-          return 100;
-        }
-      });
-      return () => clearInterval(interval);
-    }, updateInterval);
-  }, []);
-
+export const LoadingModal = ({ progress }: { progress: number }) => {
   return (
     <>
       <p className="text-dark-grafiti-medium text-center text-lg font-bold mt-2">
@@ -35,10 +7,10 @@ export const LoadingModal = ({
       <div className="relative overflow-hidden w-full max-w-[350px] bg-gray-200 rounded-full">
         <div
           className={`bg-dark-grafiti-medium h-full text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full transition-all duration-500`}
-          style={{ width: `${progress.toFixed(0)}%` }}
+          style={{ width: `${progress}%` }}
         >
           {" "}
-          {progress.toFixed(0)}%
+          {progress}%
         </div>
       </div>
       <p className="text-dark-grafiti-light text-base font-semibold text-center">
--- packages/sdk/src/interfaces/snark-proof.ts
+++ packages/sdk/src/interfaces/snark-proof.ts
@@ -47,3 +47,6 @@ export interface ParseNoteInterface {
   nullifier: string;
   account_hash: string;
 }
+export interface Logger {
+  debug(message: string): void;
+}
