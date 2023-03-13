098004ba63376eaa5d29eba2c57152938b105ef2
Mateus Santana <42101423+1Mateus@users.noreply.github.com>
2023-03-13 17:51:43 -0300

    feat(relayer): requested changes

--- packages/relayer/src/interfaces/env.ts
+++ packages/relayer/src/interfaces/env.ts
@@ -10,5 +10,6 @@ export interface Env {
   RELAYER_FEE: string;
   RELAYER_URL: string;
   HYC_CONTRACT: string;
+  // Storage fee charged for relaying (0.25 == 25%)
   BASE_STORAGE_FEE: string;
 }
--- packages/sdk/src/constants/near.ts
+++ packages/sdk/src/constants/near.ts
@@ -2,5 +2,5 @@
  * Near constants
  */
 export const AttachedGas = "300000000000000";
-export const OneYOctoNear = "1000000000000000000000000";
+export const OneNear = "1000000000000000000000000";
 export const nearNominationExp = 24;
