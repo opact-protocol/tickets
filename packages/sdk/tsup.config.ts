47513c5295c2b0ae9169f388e8ea6f5fa7f4361f
Alexandre <alexandre01393@gmail.com>
2023-03-17 15:05:38 -0300

    feat(sdk/relayer): add esbuild plugin node-modules-polyfill

--- packages/sdk/tsup.config.ts
+++ packages/sdk/tsup.config.ts
@@ -1,11 +1,12 @@
 // import path from 'path';
 import { defineConfig } from "tsup";
 import { commonjs } from "@hyrious/esbuild-plugin-commonjs";
+import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
 
 export default defineConfig({
   dts: true,
   clean: true,
-  esbuildPlugins: [commonjs()],
+  esbuildPlugins: [commonjs(), NodeModulesPolyfillPlugin()],
   format: ["cjs", "esm"],
   entry: ["src/index.ts"],
 });
