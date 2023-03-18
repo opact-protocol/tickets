0e56494ef133dd9a5ab66adef69a9a6f55058a7a
Mateus Santana <42101423+1Mateus@users.noreply.github.com>
2023-03-18 10:07:18 -0300

    feat(front): update UX

--- packages/front/src/components/modals/loading/index.tsx
+++ packages/front/src/components/modals/loading/index.tsx
@@ -7,10 +7,10 @@ export const LoadingModal = ({ progress }: { progress: number }) => {
       <div className="relative overflow-hidden w-full max-w-[350px] bg-gray-200 rounded-full">
         <div
           className={`bg-dark-grafiti-medium h-full text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full transition-all duration-500`}
-          style={{ width: `${progress}%` }}
+          style={{ width: `${progress.toFixed(0)}%` }}
         >
           {" "}
-          {progress}%
+          {progress.toFixed(0)}%
         </div>
       </div>
       <p className="text-dark-grafiti-light text-base font-semibold text-center">
