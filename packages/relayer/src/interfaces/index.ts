14412a86cfa19d2446ca59396b5403236e6d0beb
Mateus Santana <42101423+1Mateus@users.noreply.github.com>
2023-03-30 03:31:19 -0300

    feat(relayer): create fee interfaces file

--- packages/relayer/src/interfaces/index.ts
+++ packages/relayer/src/interfaces/index.ts
@@ -1,3 +1,4 @@
 export * from "./env";
 export * from "./ref";
+export * from './fee';
 export * from "./relayer";
--- packages/relayer/src/services/fee.ts
+++ packages/relayer/src/services/fee.ts
@@ -8,54 +8,12 @@ import Big from "big.js";
 import { utils } from "near-api-js";
 import { getConfig } from '@/constants';
 import jwt from "@tsndr/cloudflare-worker-jwt";
-import { Env, EstimateSwapView } from "@/interfaces";
+import { CalculateFeeRequestInterface, CalculateFeeResponseInterface, Env, EstimateSwapView, RequestParamsInterface } from "@/interfaces";
 import { fetchAllPools, estimateSwap } from '@/helpers';
 import { RouterRequest } from "@tsndr/cloudflare-worker-router";
 
 const errorStatus = 500;
 const successStatus = 200;
-
-export type CurrencyType =
-  | {
-      type: "Near";
-    }
-  | {
-      type: "Nep141";
-      account_id: string;
-    };
-
-export type CalculateFeeBodyResponseType =
-  | {
-      status: "failure";
-      error: string;
-    }
-  | {
-      token: string;
-      status: "sucess";
-      timestamp: number;
-      network_fee: string;
-      percentage_fee: string;
-      price_token_fee: string;
-      valid_fee_for_ms: number;
-      human_network_fee: string;
-      user_will_receive: string;
-      formatted_token_fee: string;
-      formatted_user_will_receive: string;
-    };
-
-export interface RequestParamsInterface {
-  instanceId: string;
-  receiverAccountId: string;
-}
-
-export interface CalculateFeeRequestInterface extends RouterRequest {
-  body: RequestParamsInterface;
-}
-
-export interface CalculateFeeResponseInterface {
-  status: number;
-  body: CalculateFeeBodyResponseType;
-}
 
 export const calculateFee = async (
   request: RouterRequest & { body: CalculateFeeRequestInterface },
