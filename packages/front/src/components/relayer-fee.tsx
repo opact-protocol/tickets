c3561d4ed4335404d82595f4b8fc2b3465e11cd7
Mateus Santana <42101423+1Mateus@users.noreply.github.com>
2023-04-04 10:46:17 -0300

    fix(front): fee countdown

--- packages/front/src/components/relayer-fee.tsx
+++ packages/front/src/components/relayer-fee.tsx
@@ -1,5 +1,6 @@
 import Countdown from "react-countdown";
-import { useRelayer } from "@/store";
+import { useModal, useRelayer, useWithdraw } from "@/store";
+import { If } from "./if";
 
 export const getHumanFormat = (value: number | string): string =>
   value < 10 ? `0${value}` : String(value);
@@ -9,6 +10,14 @@ export const RelayerFee = () => {
     dynamicFee,
   } = useRelayer();
 
+  const {
+    generatingProof,
+  } = useWithdraw();
+
+  const {
+    confirmWithdrawModal
+  } = useModal();
+
   return (
     <div className="mt-[24px] mb-4">
       <div className="flex justify-between items-center mb-3">
@@ -16,18 +25,22 @@ export const RelayerFee = () => {
           <span className="text-black font-bold">Total</span>
         </div>
 
-        <div className="text-black text-sm">
-          <Countdown
-            date={Date.now() + dynamicFee.valid_fee_for_ms}
-            key={dynamicFee.token}
-            renderer={({ hours, minutes, seconds }) => (
-              <span className="w-[65px] flex items-center">
-                {getHumanFormat(hours)}:{getHumanFormat(minutes)}:
-                {getHumanFormat(seconds)}
-              </span>
-            )}
-          />
-        </div>
+        <If
+          condition={!generatingProof && !confirmWithdrawModal}
+        >
+          <div className="text-black text-sm">
+            <Countdown
+              date={Date.now() + dynamicFee.valid_fee_for_ms}
+              key={dynamicFee.token}
+              renderer={({ hours, minutes, seconds }) => (
+                <span className="w-[65px] flex items-center">
+                  {getHumanFormat(hours)}:{getHumanFormat(minutes)}:
+                  {getHumanFormat(seconds)}
+                </span>
+              )}
+            />
+          </div>
+        </If>
       </div>
 
       <div className="flex flex-col w-full mt-2">
